c2f878cbc54108eab3c08c069c93a1a5de3cf1a3
sandreja <salimandreja@gmail.com>
2026-01-21 15:44:18 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    static int x = 5;
+    static int y = 7;
+    static String text= "OVO JE NAPISANO VELIKIM SLOVIMA";
+    public static void main(String[] args) {
+        System.out.println("zbir x i y je: "+(x+y));
+        System.out.println(text.toLowerCase());
+    }
+
+}
+
